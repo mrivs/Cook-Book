bfe94c2112ee51d7c8b4f1f136c2d03613e07ca9
mrivs <valentinilyins@gmail.com>
2024-03-11 22:16:24 +0300

    server config start+

--- project_cookbook/project_cookbook/settings.py
+++ project_cookbook/project_cookbook/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv  
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,11 +27,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zkr3c$3tzc3z63rku%2rt+4ukj_x%nxctlr=q$w&7@6hojqxz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get("SECRET_KEY")  
+
+if os.environ.get("DEBUG") == "False":
+    DEBUG = False
+else:
+    DEBUG = True
+    
+SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_SECURE = True
 
 
-# ALLOWED_HOSTS = ["127.0.0.1", "mrivs.pythonanywhere.com"]
-ALLOWED_HOSTS = ["127.0.0.1"]           # <------------------
+ALLOWED_HOSTS = ["127.0.0.1", "mrivs.pythonanywhere.com"]         # <------------------
 
 # Application definition
 
@@ -76,11 +86,18 @@ WSGI_APPLICATION = 'project_cookbook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    "default": {  
+        "ENGINE": "django.db.backends.mysql",  
+        "NAME": os.getenv("MYSQL_DBNAME"),  
+        "USER": os.getenv("MYSQL_USER"),  
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),  
+        "HOST": os.getenv("MYSQL_HOST"),  
+        "OPTIONS": {  
+            "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",  
+            "charset": "utf8mb4",  
+        },  
+    }  
 }
 
 
@@ -118,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -128,10 +145,13 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'           # <------------------
-MEDIA_ROOT = BASE_DIR / 'media' # <------------------
 
 LOGIN_REDIRECT_URL='/'          # <------------------
 LOGOUT_REDIRECT_URL='/'
 
+STATIC_URL = "static/"  
+STATIC_ROOT = BASE_DIR / "static/"  
+
+MEDIA_URL = "media/"  
+MEDIA_ROOT = BASE_DIR / "media/"
 
